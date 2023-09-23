d7c17d08e9f1f741138b0875ab6dc175eb6779e4
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2023-09-23 21:02:02 +0200

    Disallow the use of dmd, gdc, and the statically linked phobos2 library
    ~ender

--- src/bootstrap/bootstrap.cmake
+++ src/bootstrap/bootstrap.cmake
@@ -4,7 +4,21 @@ set( BOOTSTRAP_DIR ${CMAKE_CURRENT_LIST_DIR} )
 
 add_custom_target( bootstrap
 	COMMAND
-		dub build --arch=x86
+		"dub build --arch=x86"
+	DEPENDS
+		"${GAMEDIR}/bin/phobos2-ldc-shared${CMAKE_SHARED_LIBRARY_SUFFIX}"
 	WORKING_DIRECTORY
 		${BOOTSTRAP_DIR}
+)
+
+set( PHOBOS2_PATH "" )
+
+# FIXME: Finding the phobos-shared-ting ain't working: please fix
+find_path( PHOBOS2_PATH "dub${CMAKE_EXECUTABLE_SUFFIX}" PATHS "$ENV{Path}" NO_CACHE REQUIRED )
+message( FATAL_ERROR "`${PHOBOS2_PATH}`" )
+add_custom_command(
+	OUTPUT
+		"${GAMEDIR}/bin/phobos2-ldc-shared${CMAKE_SHARED_LIBRARY_SUFFIX}"
+	COMMAND
+		"${CMAKE_EXECUTABLE} -E copy_if_different ${PHOBOS2_PATH} ${GAMEDIR}/bin/phobos2-ldc-shared${CMAKE_SHARED_LIBRARY_SUFFIX}"
 )
