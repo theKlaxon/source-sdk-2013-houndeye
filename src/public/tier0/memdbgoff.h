9e8af83e36e0b11ceb05c25cfb3d133b753c7b14
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2023-12-21 12:03:53 +0100

    style: Reformat `memdbg(on|off).h`

--- src/public/tier0/memdbgoff.h
+++ src/public/tier0/memdbgoff.h
@@ -8,18 +8,16 @@
 //=============================================================================//
 
 #ifdef MEM_OVERRIDE_ON
+	#undef malloc
+	#undef realloc
+	#undef calloc
+	#undef free
+	#undef _expand
+	#undef _msize
+	#undef new
+	#undef _aligned_malloc
+	#undef _aligned_free
+	#undef _malloc_dbg
 
-#undef malloc
-#undef realloc
-#undef calloc
-#undef free
-#undef _expand
-#undef _msize
-#undef new
-#undef _aligned_malloc
-#undef _aligned_free
-#undef _malloc_dbg
-
-#undef MEM_OVERRIDE_ON
-
+	#undef MEM_OVERRIDE_ON
 #endif
--- src/public/tier0/memdbgon.h
+++ src/public/tier0/memdbgon.h
@@ -12,237 +12,218 @@
 // by including memdbgoff.h)
 
 #if !defined(STEAM) && !defined(NO_MALLOC_OVERRIDE)
+	// SPECIAL NOTE #2: This must be the final include in a .cpp or .h file!!!
 
-// SPECIAL NOTE #2: This must be the final include in a .cpp or .h file!!!
+	#if defined(_DEBUG) && !defined(USE_MEM_DEBUG)
+		#define USE_MEM_DEBUG 1
+	#endif
 
-#if defined(_DEBUG) && !defined(USE_MEM_DEBUG)
-#define USE_MEM_DEBUG 1
-#endif
+	#if defined(NO_HOOK_MALLOC)
+		#undef USE_MEM_DEBUG
+	#endif
 
-#if defined(NO_HOOK_MALLOC)
-#undef USE_MEM_DEBUG
-#endif
+	// If debug build or ndebug and not already included MS custom alloc files, or already included this file
+	#if ( defined(_DEBUG) || !defined(_INC_CRTDBG) ) || defined(MEMDBGON_H)
+	#include "basetypes.h"
+	#ifdef _WIN32
+		#include <tchar.h>
+	#else
+		#include <wchar.h>
+	#endif
+	#include <string.h>
+	#include <malloc.h>
+	#include "commonmacros.h"
+	#include "memalloc.h"
 
-// If debug build or ndebug and not already included MS custom alloc files, or already included this file
-#if (defined(_DEBUG) || !defined(_INC_CRTDBG)) || defined(MEMDBGON_H)
+	#if defined(USE_MEM_DEBUG)
+		#if defined( POSIX )
 
-#include "basetypes.h"
-#ifdef _WIN32
-#include <tchar.h>
-#else
-#include <wchar.h>
-#endif
-#include <string.h>
-#include <malloc.h>
-#include "commonmacros.h"
-#include "memalloc.h"
+			#define _NORMAL_BLOCK 1
 
-#if defined(USE_MEM_DEBUG)
-	#if defined( POSIX )
-	
-		#define _NORMAL_BLOCK 1
-		
-		#include <cstddef>
-		#include <glob.h>
-		#include <new>
-		#include <sys/types.h>
-		#if !defined( DID_THE_OPERATOR_NEW )
-                        #define DID_THE_OPERATOR_NEW
-			// posix doesn't have a new of this form, so we impl our own
-			void* operator new( size_t nSize, int blah, const char *pFileName, int nLine );
-			void* operator new[]( size_t nSize, int blah, const char *pFileName, int nLine );
+			#include <cstddef>
+			#include <glob.h>
+			#include <new>
+			#include <sys/types.h>
+			#if !defined( DID_THE_OPERATOR_NEW )
+				#define DID_THE_OPERATOR_NEW
+				// posix doesn't have a new of this form, so we impl our own
+				void* operator new( size_t nSize, int blah, const char *pFileName, int nLine );
+				void* operator new[]( size_t nSize, int blah, const char *pFileName, int nLine );
+			#endif
+
+		#else // defined(POSIX)
+			// Include crtdbg.h and make sure _DEBUG is set to 1.
+			#if !defined(_DEBUG)
+				#define _DEBUG 1
+				#include <crtdbg.h>
+				#undef _DEBUG
+			#else
+				#include <crtdbg.h>
+			#endif // !defined(_DEBUG)
+		#endif // defined(POSIX)
+	#endif
+
+	#include "tier0/memdbgoff.h"
+
+	// --------------------------------------------------------
+	// Debug/non-debug agnostic elements
+
+	#define MEM_OVERRIDE_ON 1
+
+	#undef malloc
+	#undef realloc
+	#undef calloc
+	#undef _expand
+	#undef free
+	#undef _msize
+	#undef _aligned_malloc
+	#undef _aligned_free
+
+	#ifndef MEMDBGON_H
+		inline void *MemAlloc_InlineCallocMemset( void* pMem, size_t nCount, size_t nElementSize ) {
+			memset( pMem, 0, nElementSize * nCount );
+			return pMem;
+		}
+	#endif
+
+	#define calloc(c, s)		MemAlloc_InlineCallocMemset( malloc( c * s ), c, s )
+	#define free(p)				g_pMemAlloc->Free( p )
+	#define _msize(p)			g_pMemAlloc->GetSize( p )
+	#define _expand(p, s)		_expand_NoLongerSupported(p, s)
+	#define _aligned_free( p )	MemAlloc_FreeAligned( p )
+
+	// --------------------------------------------------------
+	// Debug path
+	#if defined(USE_MEM_DEBUG)
+		#define malloc(s)				g_pMemAlloc->Alloc( s, __FILE__, __LINE__)
+		#define realloc(p, s)			g_pMemAlloc->Realloc( p, s, __FILE__, __LINE__ )
+		#define _aligned_malloc( s, a )	MemAlloc_AllocAligned( s, a, __FILE__, __LINE__ )
+
+		#define _malloc_dbg(s, t, f, l)	WHYCALLINGTHISDIRECTLY(s)
+
+		#if !defined( LINUX )
+			#if defined(__AFX_H__) && defined(DEBUG_NEW)
+				#define new DEBUG_NEW
+			#else
+				#undef new
+				#define MEMALL_DEBUG_NEW new(_NORMAL_BLOCK, __FILE__, __LINE__)
+				#define new MEMALL_DEBUG_NEW
+			#endif
 		#endif
-	
-	#else // defined(POSIX)
-	
-		// Include crtdbg.h and make sure _DEBUG is set to 1.
-		#if !defined(_DEBUG)
-			#define _DEBUG 1
-			#include <crtdbg.h>
-			#undef _DEBUG
-		#else
-			#include <crtdbg.h>
-		#endif // !defined(_DEBUG)
-	
-	#endif // defined(POSIX)
-#endif
 
-#include "tier0/memdbgoff.h"
+		#undef _strdup
+		#undef strdup
+		#undef _wcsdup
+		#undef wcsdup
 
-// --------------------------------------------------------
-// Debug/non-debug agnostic elements
+		#define _strdup(s) MemAlloc_StrDup(s, __FILE__, __LINE__)
+		#define strdup(s)  MemAlloc_StrDup(s, __FILE__, __LINE__)
+		#define _wcsdup(s) MemAlloc_WcStrDup(s, __FILE__, __LINE__)
+		#define wcsdup(s)  MemAlloc_WcStrDup(s, __FILE__, __LINE__)
 
-#define MEM_OVERRIDE_ON 1
+		// Make sure we don't define strdup twice
+		#if !defined(MEMDBGON_H)
 
-#undef malloc
-#undef realloc
-#undef calloc
-#undef _expand
-#undef free
-#undef _msize
-#undef _aligned_malloc
-#undef _aligned_free
+			inline char* MemAlloc_StrDup( const char* pString, const char* pFileName, unsigned nLine ) {
+				char *pMemory;
 
-#ifndef MEMDBGON_H
-inline void *MemAlloc_InlineCallocMemset( void *pMem, size_t nCount, size_t nElementSize)
-{
-	memset(pMem, 0, nElementSize * nCount);
-	return pMem;
-}
-#endif
+				if (! pString )
+					return NULL;
 
-#define calloc(c, s)		MemAlloc_InlineCallocMemset(malloc(c*s), c, s)
-#define free(p)				g_pMemAlloc->Free( p )
-#define _msize(p)			g_pMemAlloc->GetSize( p )
-#define _expand(p, s)		_expand_NoLongerSupported(p, s)
-#define _aligned_free( p )	MemAlloc_FreeAligned( p )
+				size_t len = strlen(pString) + 1;
+				if ( (pMemory = (char *)g_pMemAlloc->Alloc(len, pFileName, nLine)) != NULL ) {
+					return strcpy( pMemory, pString );
+				}
 
-// --------------------------------------------------------
-// Debug path
-#if defined(USE_MEM_DEBUG)
+				return NULL;
+			}
 
-#define malloc(s)				g_pMemAlloc->Alloc( s, __FILE__, __LINE__)
-#define realloc(p, s)			g_pMemAlloc->Realloc( p, s, __FILE__, __LINE__ )
-#define _aligned_malloc( s, a )	MemAlloc_AllocAligned( s, a, __FILE__, __LINE__ )
+			inline wchar_t* MemAlloc_WcStrDup( const wchar_t* pString, const char* pFileName, unsigned nLine ) {
+				wchar_t *pMemory;
 
-#define _malloc_dbg(s, t, f, l)	WHYCALLINGTHISDIRECTLY(s)
+				if (! pString )
+					return NULL;
 
-#if !defined( LINUX )
-#if defined(__AFX_H__) && defined(DEBUG_NEW)
-	#define new DEBUG_NEW
-#else
-	#undef new
-	#define MEMALL_DEBUG_NEW new(_NORMAL_BLOCK, __FILE__, __LINE__)
-	#define new MEMALL_DEBUG_NEW
-#endif
-#endif
+				size_t len = ( wcslen( pString ) + 1 );
+				if ( (pMemory = (wchar_t *)g_pMemAlloc->Alloc(len * sizeof(wchar_t), pFileName, nLine)) != NULL ) {
+					return wcscpy( pMemory, pString );
+				}
 
-#undef _strdup
-#undef strdup
-#undef _wcsdup
-#undef wcsdup
+				return NULL;
+			}
+		#endif // DBMEM_DEFINED_STRDUP
+	#else
+		// --------------------------------------------------------
+		// Release path
 
-#define _strdup(s) MemAlloc_StrDup(s, __FILE__, __LINE__)
-#define strdup(s)  MemAlloc_StrDup(s, __FILE__, __LINE__)
-#define _wcsdup(s) MemAlloc_WcStrDup(s, __FILE__, __LINE__)
-#define wcsdup(s)  MemAlloc_WcStrDup(s, __FILE__, __LINE__)
+		#define malloc(s)				g_pMemAlloc->Alloc( s )
+		#define realloc(p, s)			g_pMemAlloc->Realloc( p, s )
+		#define _aligned_malloc( s, a )	MemAlloc_AllocAligned( s, a )
 
-// Make sure we don't define strdup twice
-#if !defined(MEMDBGON_H)
+		#ifndef _malloc_dbg
+		#define _malloc_dbg(s, t, f, l)	WHYCALLINGTHISDIRECTLY(s)
+		#endif
 
-inline char *MemAlloc_StrDup(const char *pString, const char *pFileName, unsigned nLine)
-{
-	char *pMemory;
-	
-	if (!pString)
-		return NULL;
-	
-	size_t len = strlen(pString) + 1;
-	if ((pMemory = (char *)g_pMemAlloc->Alloc(len, pFileName, nLine)) != NULL)
-	{
-		return strcpy( pMemory, pString );
-	}
-	
-	return NULL;
-}
+		#undef new
 
-inline wchar_t *MemAlloc_WcStrDup(const wchar_t *pString, const char *pFileName, unsigned nLine)
-{
-	wchar_t *pMemory;
-	
-	if (!pString)
-		return NULL;
-	
-	size_t len = (wcslen(pString) + 1);
-	if ((pMemory = (wchar_t *)g_pMemAlloc->Alloc(len * sizeof(wchar_t), pFileName, nLine)) != NULL)
-	{
-		return wcscpy( pMemory, pString );
-	}
-	
-	return NULL;
-}
+		#undef _strdup
+		#undef strdup
+		#undef _wcsdup
+		#undef wcsdup
 
-#endif // DBMEM_DEFINED_STRDUP
+		#define _strdup(s) MemAlloc_StrDup(s)
+		#define strdup(s)  MemAlloc_StrDup(s)
+		#define _wcsdup(s) MemAlloc_WcStrDup(s)
+		#define wcsdup(s)  MemAlloc_WcStrDup(s)
 
-#else
-// --------------------------------------------------------
-// Release path
+		// Make sure we don't define strdup twice
+		#if !defined(MEMDBGON_H)
 
-#define malloc(s)				g_pMemAlloc->Alloc( s )
-#define realloc(p, s)			g_pMemAlloc->Realloc( p, s )
-#define _aligned_malloc( s, a )	MemAlloc_AllocAligned( s, a )
+			inline char *MemAlloc_StrDup( const char* pString ) {
+				char* pMemory;
 
-#ifndef _malloc_dbg
-#define _malloc_dbg(s, t, f, l)	WHYCALLINGTHISDIRECTLY(s)
-#endif
+				if (! pString )
+					return NULL;
 
-#undef new
+				size_t len = strlen(pString) + 1;
+				if ( (pMemory = (char *)g_pMemAlloc->Alloc(len)) != NULL ) {
+					return strcpy( pMemory, pString );
+				}
 
-#undef _strdup
-#undef strdup
-#undef _wcsdup
-#undef wcsdup
+				return NULL;
+			}
 
-#define _strdup(s) MemAlloc_StrDup(s)
-#define strdup(s)  MemAlloc_StrDup(s)
-#define _wcsdup(s) MemAlloc_WcStrDup(s)
-#define wcsdup(s)  MemAlloc_WcStrDup(s)
+			inline wchar_t* MemAlloc_WcStrDup( const wchar_t* pString ) {
+				wchar_t *pMemory;
 
-// Make sure we don't define strdup twice
-#if !defined(MEMDBGON_H)
+				if (! pString )
+					return NULL;
 
-inline char *MemAlloc_StrDup(const char *pString)
-{
-	char *pMemory;
+				size_t len = (wcslen(pString) + 1);
+				if ( (pMemory = (wchar_t *)g_pMemAlloc->Alloc(len * sizeof(wchar_t))) != NULL ) {
+					return wcscpy( pMemory, pString );
+				}
 
-	if (!pString)
-		return NULL;
+				return NULL;
+			}
 
-	size_t len = strlen(pString) + 1;
-	if ((pMemory = (char *)g_pMemAlloc->Alloc(len)) != NULL)
-	{
-		return strcpy( pMemory, pString );
-	}
+			#endif // DBMEM_DEFINED_STRDUP
 
-	return NULL;
-}
+		#endif // USE_MEM_DEBUG
 
-inline wchar_t *MemAlloc_WcStrDup(const wchar_t *pString)
-{
-	wchar_t *pMemory;
+		#define MEMDBGON_H // Defined here so can be used above
 
-	if (!pString)
-		return NULL;
-
-	size_t len = (wcslen(pString) + 1);
-	if ((pMemory = (wchar_t *)g_pMemAlloc->Alloc(len * sizeof(wchar_t))) != NULL)
-	{
-		return wcscpy( pMemory, pString );
-	}
-
-	return NULL;
-}
-
-#endif // DBMEM_DEFINED_STRDUP
-
-#endif // USE_MEM_DEBUG
-
-#define MEMDBGON_H // Defined here so can be used above
+	#else
+		#if defined(USE_MEM_DEBUG)
+			#ifdef _STATIC_LINKED
+				#pragma message ("Note: file includes crtdbg.h directly, therefore will cannot use memdbgon.h in non-debug build")
+			#else
+				#error "Error: file includes crtdbg.h directly, therefore will cannot use memdbgon.h in non-debug build. Not recoverable in static build"
+			#endif
+		#endif
+	#endif // _INC_CRTDBG
 
 #else
-
-#if defined(USE_MEM_DEBUG)
-#ifndef _STATIC_LINKED
-#pragma message ("Note: file includes crtdbg.h directly, therefore will cannot use memdbgon.h in non-debug build")
-#else
-#error "Error: file includes crtdbg.h directly, therefore will cannot use memdbgon.h in non-debug build. Not recoverable in static build"
-#endif
-#endif
-#endif // _INC_CRTDBG
-
-#else
-
-// Needed for MEM_ALLOC_CREDIT(), MemAlloc_Alloc(), etc.
-#include "memalloc.h"
-
+	// Needed for MEM_ALLOC_CREDIT(), MemAlloc_Alloc(), etc.
+	#include "memalloc.h"
 #endif // !STEAM && !NO_MALLOC_OVERRIDE
