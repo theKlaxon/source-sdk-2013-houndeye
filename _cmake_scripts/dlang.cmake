703464db7523278e01a72ad812fddd9e1c7a7169
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2023-12-21 11:54:01 +0100

    fix: Dlang compilation now works

--- _cmake_scripts/dlang.cmake
+++ _cmake_scripts/dlang.cmake
@@ -1,27 +1,31 @@
 # dlang.cmake
 
+set( LIBD_PHOBOS_NAME "${CMAKE_SHARED_LIBRARY_PREFIX}phobos2-ldc-shared${CMAKE_SHARED_LIBRARY_SUFFIX}" )
+set( LIBD_RUNTIME_NAME "${CMAKE_SHARED_LIBRARY_PREFIX}druntime-ldc-shared${CMAKE_SHARED_LIBRARY_SUFFIX}" )
+
 function( add_dlang_project NAMED Name IN ProjectDir )
 	add_custom_target( ${Name}
 		COMMAND
 			dub build --quiet --arch=x86
 		DEPENDS
-			"${GAMEDIR}/bin/phobos2-ldc-shared${CMAKE_SHARED_LIBRARY_SUFFIX}"
-			"${GAMEDIR}/bin/druntime-ldc-shared${CMAKE_SHARED_LIBRARY_SUFFIX}"
+			"${GAMEDIR}/bin/${LIBD_PHOBOS_NAME}"
+			"${GAMEDIR}/bin/${LIBD_RUNTIME_NAME}"
 		WORKING_DIRECTORY
 			${ProjectDir}
+		USES_TERMINAL
 	)
 endfunction()
 
-set( LDC2_ROOT "$ENV{LDC2_ROOT}" CACHE PATH "The path to the ldc2 root" )
-if ( WIN32 )
-	if ( LDC2_ROOT STREQUAL "" )
-		message( FATAL_ERROR "`LDC2_ROOT` was not provided, aborting." )
+block()
+	set( LDC2_RUNTIME_ROOT "$ENV{LDC2_RUNTIME_ROOT}" CACHE PATH "The path to the ldc2 runtime library root (contains `lib32` and `lib64` folders)" )
+	if ( LDC2_RUNTIME_ROOT STREQUAL "" )
+		message( FATAL_ERROR "`LDC2_RUNTIME_ROOT` was not provided, aborting." )
 	endif ()
-	set( DLIB_DIR "${LDC2_ROOT}/lib32" )
-else()
-	message( WARNING "Using D on ${CMAKE_SYSTEM_NAME} is not tested! Here be dragons." )
-	set( DLIB_DIR "/lib" )
-endif ()
+	if ( NOT WIN32 )
+		message( AUTHOR_WARNING "Using D on ${CMAKE_SYSTEM_NAME} is not tested! Here be dragons." )
+	endif ()
 
-configure_file( "${DLIB_DIR}/phobos2-ldc-shared${CMAKE_SHARED_LIBRARY_SUFFIX}" "${GAMEDIR}/bin/phobos2-ldc-shared${CMAKE_SHARED_LIBRARY_SUFFIX}" COPYONLY )
-configure_file( "${DLIB_DIR}/druntime-ldc-shared${CMAKE_SHARED_LIBRARY_SUFFIX}" "${GAMEDIR}/bin/druntime-ldc-shared${CMAKE_SHARED_LIBRARY_SUFFIX}" COPYONLY )
+	set( DLIB_DIR "${LDC2_RUNTIME_ROOT}/lib32" )
+	configure_file( "${DLIB_DIR}/${LIBD_PHOBOS_NAME}" "${GAMEDIR}/bin/${LIBD_PHOBOS_NAME}" COPYONLY )
+	configure_file( "${DLIB_DIR}/${LIBD_RUNTIME_NAME}" "${GAMEDIR}/bin/${LIBD_RUNTIME_NAME}" COPYONLY )
+endblock()
