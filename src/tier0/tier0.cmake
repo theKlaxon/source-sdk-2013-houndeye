a0f5f7ab786cb039fb55f14930290be8b762b435
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2024-01-11 01:30:22 +0100

    fix: Make the compiled binaries search for other modules in the same directory they live in, removing the need of passing `LD_LIBRARY_PATH` with the absolute bin dir path

--- src/tier0/tier0.cmake
+++ src/tier0/tier0.cmake
@@ -1,4 +1,5 @@
-add_library( tier0 IMPORTED SHARED )
-set( TIER0_NAME ${CMAKE_SHARED_LIBRARY_PREFIX}tier0${CMAKE_SHARED_LIBRARY_SUFFIX} )
-set_property( TARGET tier0 PROPERTY IMPORTED_IMPLIB "${LIBPUBLIC}/${TIER0_NAME}" )
-configure_file( "${LIBPUBLIC}/${TIER0_NAME}" "${GAMEDIR}/bin/${TIER0_NAME}" COPYONLY )
+# FIXME: Apparently declaring this makes `{vvis,vrad}_launcher` depend on the explicit path of the `libtier0.so` library, making it fail to load `{vrad,vvis}_dll.so`...??
+#add_library( tier0 IMPORTED SHARED )
+#set( TIER0_NAME ${CMAKE_SHARED_LIBRARY_PREFIX}tier0${CMAKE_SHARED_LIBRARY_SUFFIX} )
+#set_property( TARGET tier0 PROPERTY IMPORTED_IMPLIB "${LIBPUBLIC}/${TIER0_NAME}" )
+#configure_file( "${LIBPUBLIC}/${TIER0_NAME}" "${GAMEDIR}/bin/${TIER0_NAME}" COPYONLY )
