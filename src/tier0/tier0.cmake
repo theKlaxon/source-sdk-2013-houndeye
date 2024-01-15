8cc758cd52c7cb7a7ff5531ed104cb75f4e9b4ed
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2024-01-15 15:59:01 +0100

    fix: Library dynamic link error

--- src/tier0/tier0.cmake
+++ src/tier0/tier0.cmake
@@ -1,5 +1,10 @@
-# FIXME: Apparently declaring this makes `{vvis,vrad}_launcher` depend on the explicit path of the `libtier0.so` library, making it fail to load `{vrad,vvis}_dll.so`...??
-#add_library( tier0 IMPORTED SHARED )
-#set( TIER0_NAME ${CMAKE_SHARED_LIBRARY_PREFIX}tier0${CMAKE_SHARED_LIBRARY_SUFFIX} )
-#set_property( TARGET tier0 PROPERTY IMPORTED_IMPLIB "${LIBPUBLIC}/${TIER0_NAME}" )
-#configure_file( "${LIBPUBLIC}/${TIER0_NAME}" "${GAMEDIR}/bin/${TIER0_NAME}" COPYONLY )
+# tier0.cmake
+
+add_library( tier0 IMPORTED SHARED )
+set( TIER0_NAME ${CMAKE_SHARED_LIBRARY_PREFIX}tier0${CMAKE_SHARED_LIBRARY_SUFFIX} )
+set_target_properties( tier0
+	PROPERTIES
+		IMPORTED_IMPLIB "${LIBPUBLIC}/${TIER0_NAME}"
+		IMPORTED_NO_SONAME true
+)
+configure_file( "${LIBPUBLIC}/${TIER0_NAME}" "${GAMEDIR}/bin/${TIER0_NAME}" COPYONLY )
--- src/vstdlib/vstdlib.cmake
+++ src/vstdlib/vstdlib.cmake
@@ -1,5 +1,10 @@
-# FIXME: Same as `tier0`
-#add_library( vstdlib IMPORTED SHARED )
-#set( VSTDLIB_NAME ${CMAKE_SHARED_LIBRARY_PREFIX}vstdlib${CMAKE_SHARED_LIBRARY_SUFFIX} )
-#set_property( TARGET vstdlib PROPERTY IMPORTED_IMPLIB "${LIBPUBLIC}/${VSTDLIB_NAME}" )
-#configure_file( "${LIBPUBLIC}/${VSTDLIB_NAME}" "${GAMEDIR}/bin/${VSTDLIB_NAME}" COPYONLY )
+# vstdlib.cmake
+
+add_library( vstdlib IMPORTED SHARED )
+set( VSTDLIB_NAME ${CMAKE_SHARED_LIBRARY_PREFIX}vstdlib${CMAKE_SHARED_LIBRARY_SUFFIX} )
+set_target_properties( vstdlib
+	PROPERTIES
+		IMPORTED_IMPLIB "${LIBPUBLIC}/${VSTDLIB_NAME}"
+		IMPORTED_NO_SONAME true
+)
+configure_file( "${LIBPUBLIC}/${VSTDLIB_NAME}" "${GAMEDIR}/bin/${VSTDLIB_NAME}" COPYONLY )
