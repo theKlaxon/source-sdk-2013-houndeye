15262ec361b02f19fc1e06ffcb0a9457455862d5
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2024-01-16 18:17:10 +0100

    refactor: `constexpr` all the things!
    Plus small funny, which is hella performant :3

--- src/public/Color.h
+++ src/public/Color.h
@@ -1,103 +1,86 @@
 //========= Copyright Valve Corporation, All rights reserved. ============//
 //
-// Purpose: 
+// Purpose:
 //
 // $NoKeywords: $
 //=============================================================================//
-
-#ifndef COLOR_H
-#define COLOR_H
-
-#ifdef _WIN32
 #pragma once
-#endif
+
+#include <type_traits>
 
 //-----------------------------------------------------------------------------
-// Purpose: Basic handler for an rgb set of colors
+// Purpose: Basic handler for a rgb set of colors
 //			This class is fully inline
 //-----------------------------------------------------------------------------
-class Color
-{
+class Color {
 public:
 	// constructors
-	Color()
-	{
-		*((int *)this) = 0;
+	constexpr Color() = default;
+	constexpr Color( int _r, int _g, int _b ) {
+		this->SetColor( _r, _g, _b, 0 );
 	}
-	Color(int _r,int _g,int _b)
-	{
-		SetColor(_r, _g, _b, 0);
+	constexpr Color( int _r, int _g, int _b, int _a ) {
+		this->SetColor( _r, _g, _b, _a );
 	}
-	Color(int _r,int _g,int _b,int _a)
-	{
-		SetColor(_r, _g, _b, _a);
-	}
-	
+
 	// set the color
 	// r - red component (0-255)
 	// g - green component (0-255)
 	// b - blue component (0-255)
 	// a - alpha component, controls transparency (0 - transparent, 255 - opaque);
-	void SetColor(int _r, int _g, int _b, int _a = 0)
-	{
-		_color[0] = (unsigned char)_r;
-		_color[1] = (unsigned char)_g;
-		_color[2] = (unsigned char)_b;
-		_color[3] = (unsigned char)_a;
+	constexpr void SetColor( int _r, int _g, int _b, int _a = 0 ) {
+		_color[ 0 ] = static_cast<unsigned char>( _r );
+		_color[ 1 ] = static_cast<unsigned char>( _g );
+		_color[ 2 ] = static_cast<unsigned char>( _b );
+		_color[ 3 ] = static_cast<unsigned char>( _a );
 	}
 
-	void GetColor(int &_r, int &_g, int &_b, int &_a) const
-	{
-		_r = _color[0];
-		_g = _color[1];
-		_b = _color[2];
-		_a = _color[3];
+	constexpr void GetColor( int& _r, int& _g, int& _b, int& _a ) const {
+		_r = _color[ 0 ];
+		_g = _color[ 1 ];
+		_b = _color[ 2 ];
+		_a = _color[ 3 ];
 	}
 
-	void SetRawColor( int color32 )
-	{
-		*((int *)this) = color32;
+	void SetRawColor( int color32 ) {
+		*reinterpret_cast<int*>( this ) = color32;
 	}
 
-	int GetRawColor() const
-	{
-		return *((int *)this);
+	[[nodiscard]] int GetRawColor() const {
+		return *reinterpret_cast<const int*>( this );
 	}
 
-	inline int r() const	{ return _color[0]; }
-	inline int g() const	{ return _color[1]; }
-	inline int b() const	{ return _color[2]; }
-	inline int a() const	{ return _color[3]; }
-	
-	unsigned char &operator[](int index)
-	{
-		return _color[index];
+	[[nodiscard]] inline int r() const { return _color[ 0 ]; }
+	[[nodiscard]] inline int g() const { return _color[ 1 ]; }
+	[[nodiscard]] inline int b() const { return _color[ 2 ]; }
+	[[nodiscard]] inline int a() const { return _color[ 3 ]; }
+
+	constexpr unsigned char& operator[]( int index ) {
+		return _color[ index ];
 	}
 
-	const unsigned char &operator[](int index) const
-	{
-		return _color[index];
+	// Thanks, @JJl77! this the most readable version of operator[] i've ever seen
+	template<typename Integral>
+	constexpr inline const unsigned char& operator[]( const Integral index ) const noexcept requires std::is_integral_v<Integral> || std::is_convertible_v<Integral, unsigned> {
+		return _color[ static_cast<int>( index ) ];
 	}
 
-	bool operator == (const Color &rhs) const
-	{
-		return ( *((int *)this) == *((int *)&rhs) );
+	bool operator==( const Color& rhs ) const noexcept {
+		return *reinterpret_cast<const int*>( this ) == *reinterpret_cast<const int*>( &rhs );
 	}
 
-	bool operator != (const Color &rhs) const
-	{
-		return !(operator==(rhs));
+	bool operator!=( const Color& rhs ) const {
+		return ! this->operator==( rhs );
 	}
 
-	Color &operator=( const Color &rhs )
-	{
-		SetRawColor( rhs.GetRawColor() );
+	Color& operator=( const Color& rhs ) {
+		if ( &rhs == this )
+			return *this;
+
+		this->SetRawColor( rhs.GetRawColor() );
 		return *this;
 	}
 
 private:
-	unsigned char _color[4];
+	unsigned char _color[ 4 ] { 0 };
 };
-
-
-#endif // COLOR_H
