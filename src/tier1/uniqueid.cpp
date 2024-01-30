4249dfdfe1fd783a347911bb3d1a55a7b7664fbc
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2024-01-30 17:44:31 +0100

    feat: Add UUID implementation instead of platform-dependant calls

--- src/tier1/uniqueid.cpp
+++ src/tier1/uniqueid.cpp
@@ -6,14 +6,53 @@
 //
 // Unique ID generation
 //=============================================================================//
-#include "tier0/platform.h"
-#if IsWindows() && IsPC()
-	#include <rpcdce.h> // UuidCreate
-#else
-	#include "checksum_crc.h"
-#endif
 #include "tier1/uniqueid.h"
+#include "tier0/platform.h"
 #include "tier1/utlbuffer.h"
+#include <string>
+#include <string_view>
+#include <algorithm>
+
+// Indices in the UUID string representation for each byte.
+static unsigned char encoded_pos[16] { 0, 2, 4, 6, 9, 11, 14, 16, 19, 21, 24, 26, 28, 30, 32, 34 };
+// Hex
+static const auto hex = "0123456789abcdef";
+// Hex to nibble mapping.
+static const unsigned char hex_to_nibble[256] {
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+	0x08, 0x09, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+};
+
 
 //-----------------------------------------------------------------------------
 // Creates a new unique id
@@ -28,16 +67,20 @@ void CreateUniqueId( UniqueId_t* pDest ) {
 	#endif
 }
 
-
 //-----------------------------------------------------------------------------
 // Creates a new unique id from a string representation of one
 //-----------------------------------------------------------------------------
 bool UniqueIdFromString( UniqueId_t* pDest, const char* pBuf, int nMaxLen ) {
 	if ( nMaxLen == 0 ) {
 		nMaxLen = Q_strlen( pBuf );
+
+		// we got an empty string..?
+		if ( nMaxLen == 0 ) {
+			return false;
+		}
 	}
 
-	char* pTemp = (char*) stackalloc( nMaxLen + 1 );
+	auto* pTemp{ static_cast<char*>( stackalloc( nMaxLen + 1 ) ) };
 	V_strncpy( pTemp, pBuf, nMaxLen + 1 );
 	--nMaxLen;
 	while ( ( nMaxLen >= 0 ) && isspace( pTemp[ nMaxLen ] ) ) {
@@ -49,25 +92,20 @@ bool UniqueIdFromString( UniqueId_t* pDest, const char* pBuf, int nMaxLen ) {
 		++pTemp;
 	}
 
-	#if IsWindows() && IsPC()
-		Assert( sizeof( UUID ) == sizeof( *pDest ) );
+	// ensure its a valid UUID
+	auto len{ Q_strlen( pTemp ) };
+	if ( len != 36 || pTemp[8] != '-' || pTemp[13] != '-' || pTemp[18] != '-' || pTemp[23] != '-' ) {
+		return false;
+	}
 
-		if ( RPC_S_OK != UuidFromString( (unsigned char*) pTemp, (UUID*) pDest ) ) {
-			InvalidateUniqueId( pDest );
+	for ( auto i{0}; i < sizeof( encoded_pos ); i += 1 ) {
+		auto hi{ hex_to_nibble[ pTemp[ encoded_pos[i] + 0 ] ] };
+		auto lo{ hex_to_nibble[ pTemp[ encoded_pos[i] + 1 ] ] };
+		if (hi == 0xff or lo == 0xff) {
 			return false;
 		}
-	#else
-		// X360TBD: Need a real UUID Implementation
-		// For now, use crc to generate a unique ID from the UUID string.
-		Q_memset( pDest, 0, sizeof( UniqueId_t ) );
-		if ( nMaxLen > 0 ) {
-			CRC32_t crc;
-			CRC32_Init( &crc );
-			CRC32_ProcessBuffer( &crc, pBuf, nMaxLen );
-			CRC32_Final( &crc );
-			Q_memcpy( pDest, &crc, sizeof( CRC32_t ) );
-		}
-	#endif
+		pDest->m_Value[i] = hi << 4 | lo;
+	}
 
 	return true;
 }
@@ -81,8 +119,7 @@ void InvalidateUniqueId( UniqueId_t* pDest ) {
 }
 
 bool IsUniqueIdValid( const UniqueId_t& id ) {
-	UniqueId_t invalidId;
-	memset( &invalidId, 0, sizeof( UniqueId_t ) );
+	UniqueId_t invalidId{}; // zero-initialization ensures this is empty/invalid
 	return !IsUniqueIdEqual( invalidId, id );
 }
 
@@ -93,18 +130,15 @@ bool IsUniqueIdEqual( const UniqueId_t& id1, const UniqueId_t& id2 ) {
 void UniqueIdToString( const UniqueId_t& id, char* pBuf, int nMaxLen ) {
 	pBuf[ 0 ] = 0;
 
-	// X360TBD: Need a real UUID Implementation
-	#if IsWindows() && IsPC()
-		UUID* self = (UUID*) &id;
+	char string[36] { 0 };
+	string[8] = string[13] = string[18] = string[23] = '-';
 
-		unsigned char* outstring = NULL;
+	for ( auto i{ 0 }; i < sizeof( encoded_pos ); i += 1 ) {
+		string[encoded_pos[i] + 0] = hex[id.m_Value[i] >> 4  ];
+		string[encoded_pos[i] + 1] = hex[id.m_Value[i] & 0x0f];
+	}
 
-		UuidToString( self, &outstring );
-		if ( outstring && *outstring ) {
-			Q_strncpy( pBuf, (const char*) outstring, nMaxLen );
-			RpcStringFree( &outstring );
-		}
-	#endif
+	Q_strncpy( pBuf, string, std::min( 36, nMaxLen ) );
 }
 
 void CopyUniqueId( const UniqueId_t& src, UniqueId_t* pDest ) {
@@ -112,33 +146,26 @@ void CopyUniqueId( const UniqueId_t& src, UniqueId_t* pDest ) {
 }
 
 bool Serialize( CUtlBuffer& buf, const UniqueId_t& src ) {
-	// X360TBD: Need a real UUID Implementation
-	#if IsWindows() && IsPC()
-		if ( buf.IsText() ) {
-			UUID* pId = (UUID*) &src;
+	if ( buf.IsText() ) {
+		char string[36] { 0 };
+		string[8] = string[13] = string[18] = string[23] = '-';
 
-			unsigned char* outstring = NULL;
-
-			UuidToString( pId, &outstring );
-			if ( outstring && *outstring ) {
-				buf.PutString( (const char*) outstring );
-				RpcStringFree( &outstring );
-			} else {
-				buf.PutChar( '\0' );
-			}
-		} else {
-			buf.Put( &src, sizeof( UniqueId_t ) );
+		for ( auto i{ 0 }; i < sizeof( encoded_pos ); i += 1 ) {
+			string[encoded_pos[i] + 0] = hex[src.m_Value[i] >> 4  ];
+			string[encoded_pos[i] + 1] = hex[src.m_Value[i] & 0x0f];
 		}
-		return buf.IsValid();
-	#else
-		return false;
-	#endif
+
+		buf.Put( string, sizeof( string ) );
+	} else {
+		buf.Put( &src, sizeof( UniqueId_t ) );
+	}
+	return buf.IsValid();
 }
 
 bool Unserialize( CUtlBuffer& buf, UniqueId_t& dest ) {
 	if ( buf.IsText() ) {
 		int nTextLen = buf.PeekStringLength();
-		char* pBuf = (char*) stackalloc( nTextLen );
+		auto* pBuf = static_cast<char*>( stackalloc( nTextLen ) );
 		buf.GetStringManualCharCount( pBuf, nTextLen );
 		UniqueIdFromString( &dest, pBuf, nTextLen );
 	} else {
