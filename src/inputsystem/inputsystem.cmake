440a9d5dd9e193283d67cd92bee44b53f100596e
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2024-01-31 18:41:48 +0100

    style+fix: Reorder links, fix styling

--- src/inputsystem/inputsystem.cmake
+++ src/inputsystem/inputsystem.cmake
@@ -11,22 +11,22 @@ set(
 
 add_library( inputsystem MODULE ${INPUTSYSTEM_SOURCE_FILES} )
 
-set_target_properties(
-	inputsystem PROPERTIES
+set_target_properties( inputsystem
+	PROPERTIES
 		LIBRARY_OUTPUT_DIRECTORY "${GAMEDIR}/bin"
 )
 
-target_include_directories(
-	inputsystem PRIVATE
+target_include_directories( inputsystem
+	PRIVATE
 		"${SRCDIR}/common"
 		"${SRCDIR}/public"
 )
-target_link_libraries(
-	inputsystem PRIVATE
-		appframework
+target_link_libraries( inputsystem
+	PRIVATE
+#		appframework
 		tier0
 		tier1
-		tier2
 		vstdlib
+		tier2
 		SDL3::SDL3-shared
 )
