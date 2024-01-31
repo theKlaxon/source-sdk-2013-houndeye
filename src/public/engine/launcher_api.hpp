4df8aec7b1ee1d15899b01b4cf2ac56a79ca7a5a
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2024-01-31 02:59:51 +0100

    refactor: Move  engine_hlds_api.h to engine folder and strip prefix, add client equivalent (hopefully)

---
+++ src/public/engine/launcher_api.hpp
@@ -0,0 +1,14 @@
+//========= Copyright Valve Corporation, All rights reserved. ============//
+//
+// Purpose:
+//
+//=============================================================================//
+#pragma once
+
+#include "appframework/IAppSystem.h"
+#include "interface.h"
+
+
+#define VENGINE_LAUNCHER_API_VERSION "VENGINE_LAUNCHER_API_VERSION004"
+
+// TODO: Figure this out
