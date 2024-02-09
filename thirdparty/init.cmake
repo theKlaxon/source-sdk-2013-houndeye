510f1fd9948b54fbc48188a69ea81d4179b5017b
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2024-02-09 18:11:42 +0100

    fix: Actually check for submodule contents

--- thirdparty/init.cmake
+++ thirdparty/init.cmake
@@ -2,10 +2,9 @@
 
 # Function to add a git module as project
 function( included name folder )
-	# check for existance
-	if ( NOT EXISTS "${CMAKE_CURRENT_LIST_DIR}/${folder}" )
-		message( FATAL_ERROR "Thirdparty library ${name} was not found! Did you clone with `--recursive`?" )
-		return()
+	# check for existence
+	if ( NOT EXISTS "${CMAKE_CURRENT_LIST_DIR}/${folder}/CMakeLists.txt" )
+		message( FATAL_ERROR "Third-party submodule library `${name}` was not found! Did you clone with `--recursive`?\nNote: you may run `git submodule init && git submodule update` to fix" )
 	endif()
 
 	add_subdirectory( "${CMAKE_CURRENT_LIST_DIR}/${folder}" EXCLUDE_FROM_ALL SYSTEM )
