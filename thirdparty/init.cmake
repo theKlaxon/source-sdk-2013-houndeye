1b5b56694ed5a8aabc85704d552c04e7a7bde416
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2024-02-13 16:43:43 +0100

    feat: Add VPKEdit as submodule dependency
    This also disables unused subsystems in SDL, as they're only "bloat" for our purposes, it shouldn't matter to too much.
    VPKEdit was also stripped down, the GUI, CLI and installer modules have been disabled, together with their dependencies (hopefully).

--- thirdparty/init.cmake
+++ thirdparty/init.cmake
@@ -12,4 +12,23 @@ endfunction()
 
 
 # For gmodules, declare them here, for vcpkg deps, declare them directly in the dependant project
+# ----- SDL -----
+set( SDL_STATIC OFF )
+set( SDL_SHARED ON )
+set( SDL_ATOMIC OFF )
+set( SDL_AUDIO OFF )
+set( SDL_RENDER OFF )
+set( SDL_HIDAPI OFF )
+set( SDL_POWER OFF )
+set( SDL_FILE OFF )
+set( SDL_LOADSO OFF )
+set( SDL_FILESYSTEM OFF )
+set( SDL_SENSOR OFF )
+set( SDL_LOCALE OFF )
+set( SDL_MISC OFF )
 included( SDL "SDL" )
+# ----- VPKEdit -----
+set( VPKEDIT_BUILD_CLI OFF )
+set( VPKEDIT_BUILD_GUI OFF )
+set( VPKEDIT_BUILD_INSTALLER OFF )
+included( VPKEdit "VPKEdit" )
