26fc8d57707e382b7b036aa7b9ede894b2a0e144
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2024-02-13 11:06:15 +0100

    style: Un-intent sources

--- src/appframework/appframework.cmake
+++ src/appframework/appframework.cmake
@@ -1,24 +1,23 @@
 # inputsystem.cmake
 
 set( APPFRAMEWORK_DIR ${CMAKE_CURRENT_LIST_DIR} )
-set(
-	APPFRAMEWORK_SOURCE_FILES
-		# Sources
-		"${APPFRAMEWORK_DIR}/iappsystemgroup.cpp"
+set( APPFRAMEWORK_SOURCE_FILES
+	# Sources
+	"${APPFRAMEWORK_DIR}/iappsystemgroup.cpp"
 
-		# Public units
-		"${SRCDIR}/public/filesystem_init.cpp"
+	# Public units
+	"${SRCDIR}/public/filesystem_init.cpp"
 
-		# Public headers
-		"${SRCDIR}/public/filesystem_init.h"
-		"${SRCDIR}/public/appframework/AppFramework.h"
-		"${SRCDIR}/public/appframework/IAppSystem.h"
-		"${SRCDIR}/public/appframework/IAppSystemGroup.h"
-		"${SRCDIR}/public/appframework/AppFramework.h"
-		"${SRCDIR}/public/appframework/ilaunchermgr.h"
-		"${SRCDIR}/public/appframework/tier2app.h"
-		"${SRCDIR}/public/appframework/tier3app.h"
-		"${SRCDIR}/public/appframework/VguiMatSysApp.h"
+	# Public headers
+	"${SRCDIR}/public/filesystem_init.h"
+	"${SRCDIR}/public/appframework/AppFramework.h"
+	"${SRCDIR}/public/appframework/IAppSystem.h"
+	"${SRCDIR}/public/appframework/IAppSystemGroup.h"
+	"${SRCDIR}/public/appframework/AppFramework.h"
+	"${SRCDIR}/public/appframework/ilaunchermgr.h"
+	"${SRCDIR}/public/appframework/tier2app.h"
+	"${SRCDIR}/public/appframework/tier3app.h"
+	"${SRCDIR}/public/appframework/VguiMatSysApp.h"
 )
 
 add_library( appframework STATIC ${APPFRAMEWORK_SOURCE_FILES} )
