80233116933f5b085f0fc38721a9774206a326e1
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2024-03-19 15:50:47 +0100

    fix: Address `FIXME`s in `CUniformRandomStream`

--- src/vstdlib/random.cpp
+++ src/vstdlib/random.cpp
@@ -2,19 +2,7 @@
 
 IUniformRandomStream* g_pUniformRandomStream;
 
-void RandomSeed( int iSeed ) {
-    g_pUniformRandomStream->SetSeed( iSeed );
-}
-float RandomFloat( float flMinVal, float flMaxVal ) {
-    g_pUniformRandomStream->RandomFloat( flMinVal, flMaxVal );
-}
-float RandomFloatExp( float flMinVal, float flMaxVal, float flExponent ) {
-	g_pUniformRandomStream->RandomFloatExp( flMinVal, flMaxVal, flExponent );
-}
-int RandomInt( int iMinVal, int iMaxVal ) {
-    g_pUniformRandomStream->RandomInt( iMinVal, iMaxVal );
-}
-float RandomGaussianFloat( float flMean, float flStdDev );
+
 
 // --- CUniformRandomStream ---
 static const uint64_t multiplier{ 6364136223846793005u };
@@ -27,15 +15,13 @@ void CUniformRandomStream::SetSeed( int iSeed ) {
 }
 
 float CUniformRandomStream::RandomFloat( float flMinVal, float flMaxVal ) {
-	// FIXME?: There is a non-zero chance of getting a zero in the divider...
-	double res;
-	do res = static_cast<double>( this->GenerateRandomNumber() ) / this->GenerateRandomNumber();
-	while ( res < flMinVal || res > flMaxVal );
+	// map from range of `0 ..= UINT32::MAX` to range of `flMin ..= flMax`
+	auto res{ flMaxVal + static_cast<double>( this->GenerateRandomNumber() - 0 ) * ( flMaxVal - flMinVal ) / ( std::numeric_limits<uint32_t>::max() - 0 ) };
 	return static_cast<float>( res );
 }
 int CUniformRandomStream::RandomInt( int iMinVal, int iMaxVal ) {
 	// NOTE: For selecting within a range we use code "inspired" from [here](https://www.pcg-random.org/posts/bounded-rands.html)
-	uint32_t bound = (iMaxVal - iMinVal) + 1;
+	uint32_t bound = ( iMaxVal - iMinVal ) + 1;
 	uint32_t threshold = -bound % bound;
 
 	int res;
@@ -45,17 +31,15 @@ int CUniformRandomStream::RandomInt( int iMinVal, int iMaxVal ) {
 	return static_cast<int>( res % bound ) + iMinVal;
 }
 float CUniformRandomStream::RandomFloatExp( float flMinVal, float flMaxVal, float flExponent ) {
-	// FIXME?: Same as `CUniformRandomStream::RandomFloat()`
-	double res;
-	do res = static_cast<double>( this->GenerateRandomNumber() ) / this->GenerateRandomNumber();
-	while ( res < flMinVal || res > flMaxVal );
-	return static_cast<float>( pow( res, flExponent ) );
+	// map from range of `0 ..= UINT32::MAX` to range of `flMin ..= flMax`
+	auto res{ flMaxVal + pow( static_cast<double>( this->GenerateRandomNumber() - 0 ), flExponent ) * ( flMaxVal - flMinVal ) / ( std::numeric_limits<uint32_t>::max() - 0 ) };
+	return static_cast<float>( res );
 }
 
 int CUniformRandomStream::GenerateRandomNumber() {
-	// https://en.wikipedia.org/wiki/Permuted_congruential_generator
-	static constexpr auto rotr32 = [](uint32_t x, unsigned r) constexpr -> uint32_t {
-		return x >> r | x << (-r & 31);
+	// NOTE: Adapted from https://en.wikipedia.org/wiki/Permuted_congruential_generator
+	static constexpr auto rotr32 = []( uint32_t x, unsigned r ) constexpr -> uint32_t {
+		return x >> r | x << ( -r & 31 );
 	};
 
 	uint64_t x{ this->m_liState };
@@ -67,8 +51,24 @@ int CUniformRandomStream::GenerateRandomNumber() {
 	return static_cast<int>( rotr32( static_cast<uint32_t>( x >> 27 ), count ) );
 }
 
+// --- CGaussianRandomStream ---
+
 
 // --- globals ---
+void RandomSeed( int iSeed ) {
+	g_pUniformRandomStream->SetSeed( iSeed );
+}
+float RandomFloat( float flMinVal, float flMaxVal ) {
+	g_pUniformRandomStream->RandomFloat( flMinVal, flMaxVal );
+}
+float RandomFloatExp( float flMinVal, float flMaxVal, float flExponent ) {
+	g_pUniformRandomStream->RandomFloatExp( flMinVal, flMaxVal, flExponent );
+}
+int RandomInt( int iMinVal, int iMaxVal ) {
+	g_pUniformRandomStream->RandomInt( iMinVal, iMaxVal );
+}
+float RandomGaussianFloat( float flMean, float flStdDev );
+
 void InstallUniformRandomStream( IUniformRandomStream* pStream ) {
 	g_pUniformRandomStream = pStream;
 }
