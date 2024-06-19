470f9c5f323249ae8cae0442429f499dad32580d
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2024-06-19 16:17:50 +0200

    style: Reformat code

--- src/public/OfflineMode.h
+++ src/public/OfflineMode.h
@@ -1,29 +1,28 @@
 //========= Copyright Valve Corporation, All rights reserved. ============//
 //
-// Purpose: 
+// Purpose:
 //
 //=============================================================================//
-
+#pragma once
 #include <vgui/ISystem.h>
 #include <vgui_controls/Controls.h>
 
 #define STEAM_OFFLINE_MODE "HKEY_CURRENT_USER\\Software\\Valve\\Steam\\Offline"
 #define STEAM_AFS_MODE "HKEY_CURRENT_USER\\Software\\Valve\\Steam\\OfflineAFS"
-#define OFFLINE_FILE "Steam\\cached\\offline_" // first part of filename
+#define OFFLINE_FILE "Steam\\cached\\offline_"// first part of filename
 
-inline bool IsSteamInOfflineMode()
-{
+inline bool IsSteamInOfflineMode() {
 	int offline = 0;
 	vgui::system()->GetRegistryInteger( STEAM_OFFLINE_MODE, offline );
 	return ( offline == 1 );
-}inline bool IsSteamInAuthenticationFailSafeMode()
-{
+}
+
+inline bool IsSteamInAuthenticationFailSafeMode() {
 	int offline = 0;
 	vgui::system()->GetRegistryInteger( STEAM_AFS_MODE, offline );
 	return ( offline == 1 );
 }
 
-inline bool IsSteamGameServerBrowsingEnabled()
-{
-	return (IsSteamInAuthenticationFailSafeMode() || !IsSteamInOfflineMode());
+inline bool IsSteamGameServerBrowsingEnabled() {
+	return ( IsSteamInAuthenticationFailSafeMode() || !IsSteamInOfflineMode() );
 }
